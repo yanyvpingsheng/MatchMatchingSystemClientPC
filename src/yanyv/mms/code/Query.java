e409a06d8582047e6185f44b40b9459cd54fb829
烟雨平生 <yanyvpingsheng@qq.com>
2020-11-05 21:52:13 +0800

    add 从服务端工程同步响应代码

---
+++ src/yanyv/mms/code/Query.java
@@ -0,0 +1,8 @@
+package yanyv.mms.code;
+
+public class Query {
+	public static final int QUERY_SUCCESS = 200;
+	public static final int NOT_EXIST = 400;
+	public static final int QUERY_NO_ID = 500;
+	public static final int DB_ERR = 600;
+}
